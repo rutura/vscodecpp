1b8a82e218fe745907e5382c4df1aa171c168bef
Daniel Gakwaya <wayadn88@gmail.com>
2024-01-12 11:04:47 +0200

    Update cmake version and bullet points

--- 3.CMakeWithPresets/main.cpp
+++ 3.CMakeWithPresets/main.cpp
@@ -1,15 +1,16 @@
 /*
-    . CMake puts the project together for you.
+    . Using cmake presets
+        . CMake puts the project together for you.
     . Pros: 
         . Relatively simple
         . Easy to set up a debugger
         . Relatively easy to bring in third party dependencies through FetchContent.
         . Easy to share project specific settings among developers and IDEs.    
-            . Show how the project can be loaded into Visual Studio.
+            . The project can easily be loaded into IDEs that understand presets
 
     . Cons:
         . Requires CMake to be installed on the system.
-        . Hard to share project specific settings among developers.
+        . May optionally require tools like Ninja to be installed on the system.
 */
 
 #include <fmt/format.h>
--- 4.CMakeWithCppModules/main.cpp
+++ 4.CMakeWithCppModules/main.cpp
@@ -2,15 +2,14 @@
     . Using cmake presets with c++20 modules
     . On windows with visual studio, be sure to explicitly use the visual studio generator.
     . Pros:
-        . Relatively simple
-        . Easy to set up a debugger
-        . Relatively easy to bring in third party dependencies through
-   FetchContent. . Easy to share project specific settings among developers and
-   IDEs. . Show how the project can be loaded into Visual Studio.
+        . Easy to set up a debugger.
+        . Relatively easy to bring in third party dependencies through FetchContent. 
+       . Easy to share project specific settings among developers and IDEs.
 
     . Cons:
         . Requires CMake to be installed on the system.
-        . Hard to share project specific settings among developers.
+        . CMake 3.28 or higher is required.
+        . Currently works out of the box for MSVC and Clang16+.
 */
 
 #include <fmt/format.h>
